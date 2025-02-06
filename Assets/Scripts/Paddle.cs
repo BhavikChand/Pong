982abd82205e4c20181264aa3fa8f25a9f81eeca
BhavikChand <hyruka98@gmail.com>
2025-02-05 22:42:34 -0800

    Paddle New Input System In Progress
    Currently I'm logging the error:
    paddleAction is null!
    UnityEngine.Debug:LogError (object)
    Paddle:Update () (at Assets/Scripts/Paddle.cs:39)
    
    Still trying to figure this out.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,27 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    public float maxPaddleSpeed = 20f;
-    public float paddleForce = 1f;
+    public Rigidbody rb;
+    public float maxPaddleSpeed = 8f;
+    private Vector3 paddleDirection;
+    public InputActionReference paddleAction;
     
     // Start is called before the first frame update
-    void Start()
+    // void Start()
+    // {
+    //     paddleDirection = paddleAction.action.ReadValue<Vector3>();
+    // }
+    private void Awake()
     {
-        
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        float movementAxis = Input.GetAxis("LeftPaddle");
-        Transform paddleTransform = GetComponent<Transform>();
+        // float movementAxis = Input.GetAxis("LeftPaddle");
+        // Transform paddleTransform = GetComponent<Transform>();
+        //
+        // Vector3 newPosition = paddleTransform.position + new Vector3(0f, 0f, movementAxis * maxPaddleSpeed * Time.deltaTime);
+        // newPosition.z = Math.Clamp(newPosition.z, -2.2f, -2.2f);
+        //
+        // paddleTransform.position = newPosition;
 
-        Vector3 newPosition = paddleTransform.position + new Vector3(0f, 0f, movementAxis * maxPaddleSpeed * Time.deltaTime);
-        newPosition.z = Math.Clamp(newPosition.z, -2.2f, -2.2f);
+        if (paddleAction == null)
+        {
+            Debug.LogError("paddleAction is null!");
+            return;
+        }
+
+        if (paddleAction.action == null)
+        {
+            Debug.LogError("paddleAction.action is null!");
+            return;
+        }
         
-        paddleTransform.position = newPosition;
+        paddleDirection = paddleAction.action.ReadValue<Vector3>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0f, 0f, paddleDirection.z * maxPaddleSpeed);
     }
 }
